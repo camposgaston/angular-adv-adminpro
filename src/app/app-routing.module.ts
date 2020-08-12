3ec2eb5b15f0bae7e3a45f323fb24298fd90f79b
Gaston Campos <camposgaston@gmail.com>
2020-08-11 23:40:34 -0300

    routing module grouping auth files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
+
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+
   { path: '**', component: NopagefoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),
-  PagesRoutingModule],
+    PagesRoutingModule,
+    AuthRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
