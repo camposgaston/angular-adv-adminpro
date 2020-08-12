54cb6181e21966a4089d75cee13d7cc8cf285274
Gaston Campos <camposgaston@gmail.com>
2020-08-11 23:19:41 -0300

    new register module grouping register and login component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
