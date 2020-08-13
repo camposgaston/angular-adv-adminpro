6ba166482e9c0b31a4447a7bb237648d7171253f
Gaston Campos <camposgaston@gmail.com>
2020-08-13 14:58:00 -0300

    change theme and check selected theme functions moved to settings service from account-settings component

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -8,39 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  // get link element id='theme' currently index.html file for changing href of theme's css
-  public linkTheme = document.querySelector('#theme');
-  // get all li with class 'selector'
-  public links: NodeListOf<Element>;
-  constructor() { }
+  private links: NodeListOf<Element>;
+
+  constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
+    // get all li with class 'selector', type: NodeListOf<Element>
     this.links = document.querySelectorAll('.selector');
-    this.checkCurrentTheme();
+    this.settingsService.checkCurrentTheme(this.links);
   }
 
   changeTheme(theme: string): void {
-    const url = `./assets/css/colors/${theme}.css`;
-    this.linkTheme.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-    this.checkCurrentTheme();
+    this.settingsService.changeTheme(theme, this.links);
   }
 
-  // check currently used theme, with 'workig' class
-  checkCurrentTheme(): void {
-
-    this.links.forEach(elem => {
-      // first remove 'woking' class
-      elem.classList.remove('working');
-      const btnTheme = elem.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
-      const currentTheme = this.linkTheme.getAttribute('href');
-      // assign class if is selected / used
-      if (btnThemeUrl === currentTheme) {
-        elem.classList.add('working');
-      }
-    });
-  }
 
 
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -5,14 +5,43 @@ import { Injectable } from '@angular/core';
 })
 export class SettingsService {
 
+  // get link element with id='theme' currently index.html file,
+  // in order to change its href link to the theme's css
   private linkTheme = document.querySelector('#theme');
 
   constructor() {
 
-    // Set theme css
+    // Sets theme css from localStorege or default
     const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
     this.linkTheme.setAttribute('href', url);
 
+  }
+
+  changeTheme(theme: string, links: NodeListOf<Element>): void {
+    // Sets given theme css
+    const url = `./assets/css/colors/${theme}.css`;
+    this.linkTheme.setAttribute('href', url);
+    // saves theme in localStorage
+    localStorage.setItem('theme', url);
+    this.checkCurrentTheme(links);
+  }
+
+
+  // check currently used theme, with 'workig' class
+  checkCurrentTheme(links: NodeListOf<Element>): void {
+
+    // get all li with class 'selector' from links: NodeListOf<Element>
+    links.forEach(elem => {
+      // first remove 'woking' class
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme.getAttribute('href');
+      // assign class if is selected / used
+      if (btnThemeUrl === currentTheme) {
+        elem.classList.add('working');
+      }
+    });
   }
 
 }
