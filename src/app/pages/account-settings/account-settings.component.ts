65ff3c425ab150b3503d954dc5904fe9c8a4a6a4
Gaston Campos <camposgaston@gmail.com>
2020-08-13 01:44:06 -0300

    check Current Theme using 'working' css class

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -7,15 +7,40 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AccountSettingsComponent implements OnInit {
+
+  // get link element id='theme' currently index.html file for changing href of theme's css
   public linkTheme = document.querySelector('#theme');
+  // get all li with class 'selector'
+  public links: NodeListOf<Element>;
   constructor() { }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
 
-  changeTheme(theme: string) {
+  changeTheme(theme: string): void {
     const url = `./assets/css/colors/${theme}.css`;
     this.linkTheme.setAttribute('href', url);
     localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
   }
+
+  // check currently used theme, with 'workig' class
+  checkCurrentTheme(): void {
+
+    this.links.forEach(elem => {
+      // first remove 'woking' class
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme.getAttribute('href');
+      // assign class if is selected / used
+      if (btnThemeUrl === currentTheme) {
+        elem.classList.add('working');
+      }
+    });
+  }
+
+
 }
