aacd3867e13e1db9d29fcb46ae56d620161110ba
Gaston Campos <camposgaston@gmail.com>
2020-08-16 01:58:49 -0300

    Promise example using fetch url

--- src/app/pages/promises/promises.component.ts
+++ src/app/pages/promises/promises.component.ts
@@ -11,21 +11,29 @@ export class PromisesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
-    const promise = new Promise((resolve, reject) => {
-      if (false) {
-        resolve('Finished Promise message');
-      } else {
-        reject('Something went wrong');
-      }
+    this.getUsers().then(usuarios => {
+      console.log(usuarios);
     });
-    promise.then((message) => {
-      console.log(message);
-    })
-      .catch(error => {
-        console.log('Promise error', error);
-      });
-    console.log('Init end');
+    // Promise example
+    // const promise = new Promise((resolve, reject) => {
+    //   if (false) { resolve('Finished Promise message'); }
+    //   else { reject('Something went wrong'); }
+    // });
+    // promise.then((message) => { console.log(message); })
+    //   .catch(error => { console.log('Promise error', error); });
+    // console.log('Init end');
+
+  }
+
+  getUsers(): any {
+    return new Promise((resolve, reject) => {
+
+
+      fetch('https://reqres.in/api/users')
+        .then(resp => resp.json())
+        .then(body => resolve(body.data));
+    });
+
   }
 
 }
