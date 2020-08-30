8fc5e4c4307b15e3e1a77697ba184eabf5508aa1
Gaston Campos <camposgaston@gmail.com>
2020-08-30 01:02:22 -0300

    Observable outside constructor

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -12,9 +12,21 @@ export class RxjsComponent {
 
   constructor() {
 
+
+
+    this.returnObservable().pipe(
+      retry(2)
+    ).subscribe(
+      valor => console.log('subs:', valor),
+      (err) => console.warn('Error:', err),
+      () => console.log('Observable finished')
+    );
+  }
+
+  returnObservable(): Observable<number> {
     let i = -1;
 
-    const obs$ = new Observable(observer => {
+    const obs$ = new Observable<number>(observer => {
 
       const interval = setInterval(() => {
 
@@ -25,20 +37,12 @@ export class RxjsComponent {
           observer.complete();
         }
         if (i === 2) {
-          i = 0;
           observer.error('i llego al valor de 2');
         }
       }, 1000);
 
     });
-
-    obs$.pipe(
-      retry(2)
-    ).subscribe(
-      valor => console.log('subs:', valor),
-      (err) => console.warn('Error:', err),
-      () => console.log('Observable finished')
-    );
+    return obs$;
   }
 
 }
