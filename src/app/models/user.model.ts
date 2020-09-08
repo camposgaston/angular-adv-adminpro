db62fa90ac0ffa3bd6870e186aa44709a337f45f
Gaston Ezequiel Campos <camposgaston@gmail.com>
2020-09-08 19:58:21 -0300

    User model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export class User {
+    constructor(
+        public name: string,
+        public lastName: string,
+        public email: string,
+        public password?: string,
+        public google?: boolean,
+        public role?: string,
+        public img?: string,
+        public uid?: string,
+    ) { }
+}
