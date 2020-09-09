9b847d47210555e1da3f02b7ca0272cd1c9329be
Gaston Ezequiel Campos <camposgaston@gmail.com>
2020-09-08 21:58:34 -0300

    register form validations

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,11 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css'
   ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    name: ['Gaston', [Validators.required, Validators.minLength(2)]],
+    lastName: ['Campos', [Validators.required, Validators.minLength(2)]],
+    email: ['camposgaston321654987@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456789', [Validators.required, Validators.minLength(8)]],
+    password2: ['123456789', [Validators.required, Validators.minLength(8)]],
+    termsAndConditions: [true, [Validators.requiredTrue]]
+  }, {
+    validators: this.samePassValidation('password', 'password2')
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  createUser() {
+    this.formSubmitted = true;
+    // console.log(this.registerForm.value);
+    console.log(this.registerForm);
+    if (this.registerForm.valid) {
+      console.log('Posteando Formulario');
+    } else {
+      console.log('Formulario no es correcto...');
+    }
+  }
+
+  notValidField(field: string): boolean {
+    if (this.registerForm.get(field).invalid && this.formSubmitted) {
+      return true;
+    }
+    return false;
+  }
+
+  samePasswords() {
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('password2').value;
+    if (pass1 !== pass2 && this.formSubmitted) {
+      return false;
+    }
+    return true;
+  }
+
+  // Equal Passwords validation
+  samePassValidation(pass1FieldName: string, pass2FieldName: string) {
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1FieldName);
+      const pass2Control = formGroup.get(pass2FieldName);
+      if (pass1Control.value === pass2Control.value) {
+        // if both are the same password, no error (null)
+        pass2Control.setErrors(null);
+      } else {
+        pass2Control.setErrors({ passNotMatch: true });
+      }
+
+      // To do: Add password strength validations here!!!!
+    };
   }
 
 }
