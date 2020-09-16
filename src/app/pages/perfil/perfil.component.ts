4f956e1f7d9e1267a3b33b728b688fd3ed5aa757
Gaston Ezequiel Campos <camposgaston@gmail.com>
2020-09-16 01:47:44 -0300

    when updating user data in db, update user data in service to update all user info. alert ussing Swal2

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
+import { User } from '../../models/user.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil',
@@ -11,14 +13,18 @@ import { UserService } from 'src/app/services/user.service';
 export class PerfilComponent implements OnInit {
 
   public profileForm: FormGroup;
+  public user: User;
 
-  constructor(private fb: FormBuilder, private userService: UserService) { }
+  constructor(private fb: FormBuilder,
+    private userService: UserService) {
+    this.user = userService.user;
+  }
 
   ngOnInit(): void {
     this.profileForm = this.fb.group({
-      name: ['Nombre', Validators.required],
-      lastName: ['Apellido', Validators.required],
-      email: ['email', [Validators.required, Validators.email]]
+      name: [this.user.name, Validators.required],
+      lastName: [this.user.lastName, Validators.required],
+      email: [this.user.email, [Validators.required, Validators.email]]
     });
   }
 
@@ -26,7 +32,11 @@ export class PerfilComponent implements OnInit {
     console.log(this.profileForm.value);
     this.userService.updateUserProfile(this.profileForm.value)
       .subscribe(resp => {
-        console.log(resp);
+        const { name, lastName, email } = this.profileForm.value;
+        this.user.name = name;
+        this.user.lastName = lastName;
+        this.user.email = email;
+        Swal.fire('Usuario modificado', 'Se modificaron los datos de usuario', 'success');
       });
   }
 
