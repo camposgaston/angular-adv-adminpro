24c8fcf35661b2d1356b7d5ab262435431956ec1
Gaston Ezequiel Campos <camposgaston@gmail.com>
2020-09-28 23:41:53 -0300

    production base_url configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // TODO change base URL in production
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://admin-pro-bknd.herokuapp.com/api'
 };
